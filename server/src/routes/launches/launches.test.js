2157084f6f6f1a538dd5a9afa38157615c361326
Aditya Yadav <adityadav023>
2022-02-23 22:27:15 +0530

    Testing API endpoint with Supertest POST

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -16,7 +16,40 @@ describe('Test GET /launches',  ()=>{
 
 describe('Test POST /launches',()=>{
 
-    test('It should respond with 200 success',()=>{
+    const completeLaunchData = {
+        mission: 'US Enterprise',
+        rocket: 'NCC1701-D',
+        target: 'Kepler - 186 f',
+        launchDate: 'January 4, 2028'
+    }
+
+    const launchDataWithoutDate = {
+        mission: 'US Enterprise',
+        rocket: 'NCC1701-D',
+        target: 'Kepler - 186 f'
+    }
+
+
+    const launchDataWithInvalidDate = {
+        mission: 'US Enterprise',
+        rocket: 'NCC1701-D',
+        target: 'Kepler - 186 f',
+        launchDate: 'wrongn'
+    }
+    test('It should respond with 201 created',async ()=>{
+        const response = await request(app)
+                            .post('/launches')
+                            .send(completeLaunchData)
+                            .expect('Content-Type',/json/)
+                            .expect(201)
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf()
+    const responseDate = new Date(response.body.launchDate).valueOf()
+
+    //checking both request and response dates are same
+    expect(responseDate).toBe(requestDate)
+    //checking reponse except date property
+    expect(response.body).toMatchObject(launchDataWithoutDate)
 
     })
 
