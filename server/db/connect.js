ac371384ff4e02c26ef24752204919e958245981
Aditya Yadav <adityadav023>
2022-03-02 17:58:30 +0530

    connected with mongoDb

---
+++ server/db/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+
+function connectDb(url){
+    return mongoose.connect(url)
+
+}
+
+module.exports = {connectDb}
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const http = require('http')
 const app = require('./app')
+const mongoose = require('mongoose')
+const {connectDb} = require('../db/connect')
 const {loadPlanetsData} = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000
@@ -7,11 +10,20 @@ const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
 
 async function startServer(){
+    await connectDb(process.env.MONGO_URL)
     await loadPlanetsData()
 
     server.listen(PORT, ()=>{
         console.log(`Server is listening on port ${PORT}`)
     })
 }
+
+mongoose.connection.once('open',()=>{
+    console.log('MongoDB connection ready!!')
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log(err)
+})
 
 startServer()
