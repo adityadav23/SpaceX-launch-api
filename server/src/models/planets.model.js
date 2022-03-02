31204ffac16138f220eda4b9d22a6ebaef7bb9be
Aditya Yadav <adityadav023>
2022-03-03 03:10:25 +0530

    Implemented getAllPlanets() and savePlanet() using find and upsert methods

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path')
 const {parse} = require('csv-parse');
-
+const planets = require('./planets.mongo')
 
 const habitablePlanets = [];
 
@@ -21,22 +21,39 @@ function isHabitablePlanet(planet) {
       }))
       .on('data', (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          savePlanet(data)
         }
       })
       .on('error', (err) => {
         console.log(err);
         reject(err)
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on('end',async () => {
+        const countPlanetsFound = (await getAllPlanets()).length
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve()
       });
   })
  }
 
- function getAllPlanets(){
-   return habitablePlanets
+ async function savePlanet(planet){
+   try{
+     await planets.updateOne({
+      keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },{
+        upsert: true,
+      })
+   }
+   catch(err){
+     console.log(`Could not save planet! ${err}`)
+   }
+ }
+
+ async function getAllPlanets(){
+   return await planets.find({})
  }
 
  module.exports ={
