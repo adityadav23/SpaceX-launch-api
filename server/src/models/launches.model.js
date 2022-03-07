534f2157dfb9157773d558309ec6bfa5d3a7d4b7
Aditya Yadav <adityadav023>
2022-03-07 19:32:56 +0530

    loaded and mapped spaceX data to our launches model using axios

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 const launches = require('./launches.mongo')
 const planets = require('./planets.mongo')
 
@@ -17,6 +19,54 @@ const launch ={
 
 // saveLaunch(launch)
 
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
+
+async function loadLaunchData(){
+    console.log('Downloading launch data...')
+    const response = await axios.post(SPACEX_API_URL,{
+        query:{},
+        options:{
+            populate:[
+                {
+                    path:'rocket',
+                    select:{
+                        name:1
+                    }
+                },{
+                    path:'payloads',
+                    select:{
+                        'customers':1
+                    }
+                }
+            ]
+        }
+    })
+
+
+    const launchDocs = response.data.docs
+
+    for(const launchDoc of launchDocs){
+        const payloads = launchDoc['payloads']
+        //getting customers form payloads
+        const customers = payloads.flatMap((payload)=>{
+            return payload['customers']
+        })
+
+     //extracting required launch data from launchDoc   
+        const launch = {
+            flightNumber:launchDoc['flight_number'],
+            mission: launchDoc['name'],
+            rocket: launchDoc['rocket']['name'],
+            launchDate: launchDoc['date_local'],
+            upcoming: launchDoc['upcoming'],
+            success: launchDoc['success'],
+            customers: customers,
+        }
+
+        console.log(`${launch.flightNumber}  ${launch.mission}`)
+    }
+
+}
 
 
 async function getAllLaunches(){
@@ -92,6 +142,7 @@ async function abortLaunchById(launchId){
 }
 
 module.exports = {getAllLaunches,
+                  loadLaunchData,  
                 scheduleNewLaunch,
                 existsLaunchWithId,
                 abortLaunchById}
